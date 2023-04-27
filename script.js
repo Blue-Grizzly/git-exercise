1f61ef7eb23682e386b3f8b9896541722afd8513
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-04-27 09:09:59 +0200

    edits

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ window.addEventListener("load", start);
 
 function start(){
 console.log("Hello world");
-console.log("its the collaborator");
+console.log("it is the collaborator");
+console.log("edit");
 }
