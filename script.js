ba6d060f9526cec17bde9fa1d93d924411ab63a3
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-04-27 09:51:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,14 @@
 window.addEventListener("load", start);
 
 function start(){
-console.log("Hello world");
-console.log("it is the collaborator");
-console.log("edit");
-console.log("just commiting and pushing");
+    logfunction();
+    console.log("Hello world");
+    console.log("it is the collaborator");
+    console.log("edit");
+    console.log("just commiting and pushing");
+}
+
+function logfunction(){
+    console.log("more lines of code")
+
 }
